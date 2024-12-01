1bb73bf9b54ddd2d8cde350a1860abfbd5f88d29
Rhema Bales <rhemabales@icloud.com>
2024-12-01 19:04:08 +0100

    add py & js paths

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"text/template"
 
 	"github.com/carlmjohnson/gateway"
 )
@@ -26,6 +27,7 @@ func main() {
 		http.Handle("/", http.FileServer(http.Dir("./static")))
 	}
 	http.HandleFunc("/api", apiRoute)
+	http.HandleFunc("/js", jsPage)
 
 	log.Fatal(listener(portStr, nil))
 }
@@ -40,4 +42,28 @@ func apiRoute(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 	w.Write(jsonRes)
+}
+
+func jsPage(w http.ResponseWriter, r *http.Request) {
+	htmlPage := `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Go Web App on Netlify</title>
+    <style>
+  	body {background: grey;}
+    </style>
+</head>
+<body>
+    <h1>Go Web App on Netlify</h1>
+    <h4>Super Speedy Website</h4>
+    <h4>{{.}}</h4>
+</body>
+</html>
+`
+	var htmlTemplate = template.Must(template.New("").Parse(htmlPage))
+	htmlTemplate.Execute(w, r.Host+"/js")
 }
